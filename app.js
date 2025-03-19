aafe7bc7be53a4d7c73ff17b802378e85f2bf47d
Jorge_Luis <jlpseveriche@gmail.com>
2025-03-18 22:20:46 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    // Agregar el nombre a la lista
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> ${amigoSecreto} es el amigo secreto </li>`;
+}
